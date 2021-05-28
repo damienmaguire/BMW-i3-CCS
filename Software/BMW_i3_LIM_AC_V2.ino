e53d0b5cbcf3e54f7308cfa2bf1ff5e4e99b4ab2
Damien Maguire <listerlt1@gmail.com>
2021-05-28 12:16:33 +0100

    Add v2
    Better control and signal descriptions.

---
+++ Software/BMW_i3_LIM_AC_V2.ino
@@ -0,0 +1,376 @@
+/*
+BMW i3 LIM  Controller Software. Alpha version.
+Version 2 with extra commands to the LIM
+0x3E9 is the most important message to send to the LIM!
+
+Copyright 2021 
+Damien Maguire
+
+//////////////////////////////////////////////////////////////
+Signal that seem to be required for AC :
+0x112 - BMS
+0x12f - Wake up KL15/KL30
+0x2FC
+0x3E9 - LIM command
+0x431 - possible battery status message?
+0x560
+
+//////////////////////////////////////////////////////////////
+Signal that seem to be required for DC CCS :
+0x3c
+0x112 - BMS
+0x12f - Wake up KL15/KL30
+0x2A0
+0x2F1 - Lim command used only for DC
+0x2FA - Lim command used only for DC
+0x2FC
+0x328 - DLC6 seems to be 1 sec interval and counting up every interval. Time of day / date? Used by LIM for charge time perhaps?
+0x330
+0x397
+0x3A0
+0x3E8
+0x3E9 - LIM command
+0x3F9
+0x431 - possible battery status message?
+0x432 - another voltage and possible SOC in this msg.
+0x510
+0x512
+0x51A
+0x540
+0x560
+/////////////////////////////////////////////////////////////
+*/
+#include <Metro.h>
+#include <due_can.h>  
+#include <due_wire.h> 
+#include <DueTimer.h>  
+#include <Wire_EEPROM.h> 
+
+
+
+#define SerialDEBUG SerialUSB
+ template<class T> inline Print &operator <<(Print &obj, T arg) { obj.print(arg); return obj; } //Allow streaming
+ 
+
+#define OUT1 48
+#define OUT2 49
+#define IN1 6
+#define IN2 7
+#define led 13
+
+Metro timer_diag = Metro(1100);
+Metro timer_Frames600 = Metro(600);
+Metro timer_Frames200 = Metro(200);
+Metro timer_Frames10 = Metro(10);
+Metro timer_Frames100 = Metro(100);
+
+
+bool CHGState=false;
+bool CHGreq=false;
+bool PP=false;
+bool Auto=false;
+byte ACcur=0;
+byte CABlim=0;
+byte Pilot=0;
+uint16_t Batt_Wh=21000; //battery energy capacity in watt hours. Values taken from 2014 and 2015 i3. Really need a 94ah or 120ah to verify ...
+uint16_t CHG_Pwr=0; //calculated charge power. 12 bit value scale x25. Values based on 50kw DC fc and 1kw and 3kw ac logs. From bms???
+int16_t FC_Cur=0; //10 bit signed int with the ccs dc current command.scale of 1.
+byte  EOC_Time=0x00; //end of charge time in minutes.
+byte CHG_Status=0;  //observed values 0 when not charging , 1 and transition to 2 when commanded to charge. only 4 bits used.
+                    //seems to control led colour.
+byte CHG_Req=0;  //observed values 0 when not charging , 1 when requested to charge. only 1 bit used in logs so far.
+byte CHG_Ready=0;  //indicator to the LIM that we are ready to charge. observed values 0 when not charging , 1 when commanded to charge. only 2 bits used.
+byte CONT_Ctrl=0;  //4 bits with DC ccs contactor command.
+
+
+CAN_FRAME outFrame;  //A structured variable according to due_can library for transmitting CAN data.
+CAN_FRAME inFrame;    //structure to keep inbound inFrames
+
+
+#define Status_NotRdy 0x0 //no led
+#define Status_Rdy 0x2  //pulsing blue led when on charge. 0x2. 0x1 = 1 red flash then off
+#define Req_Charge 0x1
+#define Req_EndCharge 0x0
+#define Chg_Rdy 0x1
+#define Chg_NotRdy 0x0
+
+
+
+void setup() 
+  {
+  Can0.begin(CAN_BPS_500K);   // LIM CAN
+  Can1.begin(CAN_BPS_500K);   // External CAN
+  Can0.watchFor();
+  Can1.watchFor();
+  Serial.begin(115200);  //Initialize our USB port which will always be redefined as SerialUSB to use the Native USB port tied directly to the SAM3X processor.
+  Serial2.begin(19200); //setup serial 2 for wifi access
+  pinMode(OUT1,OUTPUT);
+  pinMode(OUT2,OUTPUT);
+  pinMode(led,OUTPUT);
+  pinMode(IN1,INPUT);
+  pinMode(IN2,INPUT);
+  
+  }
+
+  
+void loop()
+{ 
+checkCAN();
+  if(timer_diag.check())
+  {
+  handle_Wifi();
+  }
+
+ if(timer_Frames600.check())
+  {
+  Msgs600ms();
+ }
+
+
+  if(timer_Frames10.check())
+  {
+  Msgs10ms();
+ }
+
+  if(timer_Frames100.check())
+  {
+  Msgs100ms();
+ }
+
+  if(timer_Frames200.check())
+  {
+   Msgs200ms();
+
+checkforinput();
+
+}
+}
+
+
+void Msgs10ms()                       //10ms messages here
+{
+                                  
+        outFrame.id = 0x112;       
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=0xf9;  
+        outFrame.data.bytes[1]=0x1f; 
+        outFrame.data.bytes[2]=0x8b;  
+        outFrame.data.bytes[3]=0x0e; 
+        outFrame.data.bytes[4]=0xa6;
+        outFrame.data.bytes[5]=0x71;
+        outFrame.data.bytes[6]=0x65;
+        outFrame.data.bytes[7]=0x5d;  
+        Can0.sendFrame(outFrame); 
+
+      
+
+}
+    
+void Msgs100ms()                       //100ms messages here
+{
+
+        outFrame.id = 0x12f;       
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=0xf5;  
+        outFrame.data.bytes[1]=0x28; 
+        outFrame.data.bytes[2]=0x88;  
+        outFrame.data.bytes[3]=0x1d; 
+        outFrame.data.bytes[4]=0xf1;
+        outFrame.data.bytes[5]=0x35;
+        outFrame.data.bytes[6]=0x30;
+        outFrame.data.bytes[7]=0x80;  
+        Can0.sendFrame(outFrame);  
+
+}
+
+
+void Msgs200ms()                      ////200ms messages here
+{
+
+        outFrame.id = 0x3E9;       
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=lowByte(Batt_Wh);  
+        outFrame.data.bytes[1]=highByte(Batt_Wh); 
+        outFrame.data.bytes[2]=((CHG_Status<<4)|(CHG_Req));  //charge status in bits 4-7.goes to 1 then 2.8 secs later to 2. Plug locking???. Charge request in lower nibble. 1 when charging. 0 when not charging.
+        outFrame.data.bytes[3]=((lowByte(CHG_Pwr)<<4)||CHG_Ready); //charge readiness in bits 0 and 1. 1 = ready to charge.upper nibble is LSB of charge power.
+        outFrame.data.bytes[4]=highByte(CHG_Pwr);  //MSB of charge power.in this case 0x28 = 40x25 = 1000W. Probably net DC power into the Batt.
+        outFrame.data.bytes[5]=0x00;  //LSB of the DC ccs current command
+        outFrame.data.bytes[6]=0x00;  //bits 0 and 1 MSB of the DC ccs current command.Upper nibble is DC ccs contactor control. Observed in DC fc logs only.
+                                      //transitions from 0 to 2 and start of charge but 2 to 1 to 0 at end. Status and Ready operate the same as in AC logs.
+        outFrame.data.bytes[7]=EOC_Time;// end of charge timer.  
+        Can0.sendFrame(outFrame); 
+
+        outFrame.id = 0x431;       
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=0xca;  
+        outFrame.data.bytes[1]=0xff; 
+        outFrame.data.bytes[2]=0x0b;  
+        outFrame.data.bytes[3]=0x02; 
+        outFrame.data.bytes[4]=0x69;
+        outFrame.data.bytes[5]=0x26;
+        outFrame.data.bytes[6]=0xf3;
+        outFrame.data.bytes[7]=0x4b;  
+        Can0.sendFrame(outFrame);
+        
+   
+digitalWrite(led,!digitalRead(led));//blink led every time we fire this interrrupt.
+
+}
+
+void Msgs600ms()                       //600ms messages here
+{
+                                  
+        outFrame.id = 0x560;       
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=0x00;  
+        outFrame.data.bytes[1]=0x00; 
+        outFrame.data.bytes[2]=0x00;  
+        outFrame.data.bytes[3]=0x00; 
+        outFrame.data.bytes[4]=0xfe;
+        outFrame.data.bytes[5]=0x00;
+        outFrame.data.bytes[6]=0x00;
+        outFrame.data.bytes[7]=0x60;  
+        Can0.sendFrame(outFrame); 
+
+      
+
+}
+
+
+void checkCAN()
+{
+
+  if(Can0.available())
+  {
+    Can0.read(inFrame);
+  if(inFrame.id == 0x3B4)
+  {
+  ACcur = inFrame.data.bytes[0];
+  CABlim = inFrame.data.bytes[1];
+  PP = (inFrame.data.bytes[2]&0x1);
+  Pilot = (inFrame.data.bytes[4]&0xF);
+  }
+  
+}
+}
+
+
+void handle_Wifi(){
+
+        outFrame.id = 0x2fc;       //this should be every 6 secs...
+        outFrame.length = 8;            
+        outFrame.extended = 0;          
+        outFrame.rtr=1;                 
+        outFrame.data.bytes[0]=0x81;  
+        outFrame.data.bytes[1]=0x00; 
+        outFrame.data.bytes[2]=0x04;  
+        outFrame.data.bytes[3]=0xff; 
+        outFrame.data.bytes[4]=0xff;
+        outFrame.data.bytes[5]=0xff;
+        outFrame.data.bytes[6]=0xff;
+        outFrame.data.bytes[7]=0xff;  
+        Can0.sendFrame(outFrame); 
+
+if((Auto)&&(PP)) CHGreq=true;
+
+if(CHGreq)
+{
+  EOC_Time=0xFE;
+  CHG_Status=Status_Rdy;
+  CHG_Req=Req_Charge;
+  CHG_Ready=Chg_Rdy;
+  CHG_Pwr=0x2c;//just a holding value of 1kw for now.
+  
+}
+
+if(!CHGreq)
+{
+  EOC_Time=0x00;
+  CHG_Status=Status_NotRdy;
+  CHG_Req=Req_EndCharge;
+  CHG_Ready=Chg_NotRdy;
+  CHG_Pwr=0x00;
+  
+}
+
+
+  
+/*
+ * 
+ * Routine to send data to wifi on serial 2
+The information will be provided over serial to the esp8266 at 19200 baud 8n1 in the form :
+vxxx,ixxx,pxxx,mxxxx,nxxxx,oxxx,rxxx,qxxx* where :
+
+v=pack voltage (0-700Volts)
+i=current (0-1000Amps)
+p=power (0-300kw)
+m=half pack voltage (0-500volts)
+n=Amp Hours (0-300Ah)
+o=KiloWatt Hours (0-150kWh)
+r=HV Box Temp (0-100C)
+q=third pack Volts (0-500Volts)
+*=end of string
+xxx=three digit integer for each parameter eg p100 = 100kw.
+updates will be every 1000ms approx.
+
+v100,i200,p35,m3000,n4000,o20,r100,q50*
+*/
+  
+Serial2.print("v100,i200,p35,m3000,n4000,o20,r30,q50*"); //test string
+
+//digitalWrite(13,!digitalRead(13));//blink led every time we fire this interrrupt.
+SerialDEBUG.println("//////////////////EVBMW LIM CONTROLLER V1//////////////////////////////////////////////////////////");
+SerialDEBUG.print("Pilot Current=");                                                                             
+SerialDEBUG.print(ACcur);
+SerialDEBUG.println("Amps");
+SerialDEBUG.print("Cable Limit=");
+SerialDEBUG.print(CABlim);
+SerialDEBUG.println("Amps");
+if(PP) SerialDEBUG.println("Plug Inserted");
+if(!PP) SerialDEBUG.println("Unplugged");
+SerialDEBUG.print("CP Status=");
+if(Pilot==0x8) SerialDEBUG.println("None");
+if(Pilot==0x9) SerialDEBUG.println("STD AC");
+if(Pilot==0xA) SerialDEBUG.println("AC Charging");
+if(Pilot==0xC) SerialDEBUG.println("5% DC");
+if(Pilot==0xD) SerialDEBUG.println("CCS Comms");
+if(Auto) SerialDEBUG.println("Auto Chg Active");
+if(!Auto) SerialDEBUG.println("Auto Chg Deactivated");
+SerialDEBUG.println("Commands : 'c' engage charge. 'd' disengage charge. 'a' toggle auto charge");
+
+SerialDEBUG.println("//////////////////////////////////////////////////////////////////////////////////////////");
+}
+
+void checkforinput(){ 
+  //Checks for keyboard input from Native port 
+   if (SerialDEBUG.available()) 
+     {
+      int inByte = SerialDEBUG.read();
+      switch (inByte)
+         {
+          case 'c':            
+            CHGreq=true;
+            break;
+  
+          case 'd':    
+            CHGreq=false;
+            Auto=false;
+            break;
+
+          case 'a':    
+            Auto=!Auto;
+            break;
+
+      }
+}
+}
